4c2df4be79075a31a7b705323be3599e8f078bc1
reyoung@126.com <reyoung@126.com@a30f7099-0f1c-977c-82b7-a2b34129996d>
2011-01-08 13:22:57 +0000

    ExtensionSystem/pluginspec Read XML Complete and BugFix to SpecFileMaker

--- src/lib/ExtensionSystem/pluginspec.cpp
+++ src/lib/ExtensionSystem/pluginspec.cpp
@@ -29,7 +29,110 @@ PluginSpec::~PluginSpec()
 PluginSpec::PluginSpec(const QString& fn,QObject *parent) :
     QObject(parent),m_data(new PluginSpecInnerData())
 {
-    qDebug()<<"OKOK";
+    QDomDocument document;
+    QFile file(fn);
+    file.open(QFile::ReadOnly);
+    int errorColumn,errorLine;
+    QString errorStr;
+    if (!document.setContent(&file, true, &errorStr, &errorLine,
+                                &errorColumn)) {
+        qDebug()<<tr("Parse error at line %1, column %2:\n%3")
+                                .arg(errorLine)
+                                 .arg(errorColumn)
+                                 .arg(errorStr);
+        return;
+    }
 
+    QDomNode n = document.firstChild();
+    QDomElement root = n.toElement();
+    n = root.firstChild();
+    while(!n.isNull())
+    {
+        QDomElement e = n.toElement();
+        QString tn = e.tagName();
+        if(tn == "Name")
+        {
+            this->m_data->Name = e.firstChild().toText().data();
+        }
+        else if(tn=="Version")
+        {
+            this->m_data->Version = e.firstChild().toText().data();
+        }
+        else if(tn=="Author")
+        {
+            this->m_data->Author = e.firstChild().toText().data();
+        }
+        else if(tn=="License")
+        {
+            this->m_data->License = e.firstChild().toText().data();
+        }
+        else if(tn == "Category")
+        {
+            this->m_data->Category = e.firstChild().toText().data();
+        }
+        else if(tn == "CopyRight")
+        {
+            this->m_data->CopyRight = e.firstChild().toText().data();
+        }
+        else if(tn == "Description")
+        {
+            this->m_data->Description = e.firstChild().toText().data();
+        }
+        else if(tn=="Bug_Report_E-mail")
+        {
+            this->m_data->BugReportEMail = e.firstChild().toText().data();
+        }
+        else if(tn=="Dependency_List")
+        {
+            QDomNode de = e.firstChild();
+            while(!de.isNull())
+            {
+                QDomElement el = de.toElement();
+                SpecDependencyData temp;
+                temp.Name = el.attribute("Name");
+                temp.Version = el.attribute("Version");
+                this->m_data->DependencyList.append(temp);
+                de = de.nextSibling();
+            }
+        }
+        n = n.nextSibling();
+    }
+    file.close();
 }
 
+QString PluginSpec::name()const
+{
+    return this->m_data->Name;
+}
+QString PluginSpec::version()const
+{
+    return this->m_data->Version;
+}
+QString PluginSpec::author()const
+{
+    return this->m_data->Author;
+}
+QString PluginSpec::bugReportEmail()const
+{
+    return this->m_data->BugReportEMail;
+}
+QList<SpecDependencyData> PluginSpec::dependencyList()const
+{
+    return this->m_data->DependencyList;
+}
+QString PluginSpec::license()const
+{
+    return this->m_data->License;
+}
+QString PluginSpec::category()const
+{
+    return this->m_data->Category;
+}
+QString PluginSpec::copyRight()const
+{
+    return this->m_data->CopyRight;
+}
+QString PluginSpec::description()const
+{
+    return this->m_data->Description;
+}
--- src/lib/ExtensionSystem/pluginspec.h
+++ src/lib/ExtensionSystem/pluginspec.h
@@ -6,20 +6,125 @@
 #include "ExtensionSystem_global.h"
 namespace ExtensionSystem
 {
-    struct PluginSpecInnerData;
+    struct PluginSpecInnerData;/// Plugin Spec Inner Data. Plugin的成员变量
+/**
+*	表示Plugin 依赖关系的Data
+*       有依赖于的Name和相应的Version
+*
+*/
     struct SpecDependencyData
     {
         QString Name;
         QString Version;
     };
 
+/**
+*	Plugin Spec
+*       Plugin描述文件的抽象。通过描述文件，来加载Plugin
+*
+*/
     class EXTENSIONSYSTEMSHARED_EXPORT PluginSpec : public QObject
     {
         Q_OBJECT
     public:
 
+//! 默认构造函数 ,以Spec文件名为参数.
+/*!
+ \author Reyoung
+ \date 2011.01.08
+ \version ver 0.0.1
+  -
+ \exception
+ \test
+ \note
+ \attention
+ \sa
+ \remark 1.2011.01.08 21:13 created by Reyoung
+ \return
+*/
         explicit PluginSpec(const QString& fn,QObject *parent = 0);
         virtual ~PluginSpec();
+//! 返回Spec Name.
+/*!
+ \author Reyoung
+ \date 2011.01.08
+ \version ver 0.0.1
+  -
+ \exception
+ \test
+ \note
+ \attention
+ \sa
+ \remark 1.2011.01.08 21:14 created by Reyoung
+ \return QString
+*/
+        QString name()const;
+//! 返回Plugin的版本.
+/*!
+ \author Reyoung
+ \date 2011.01.08
+ \version ver 0.0.1
+  -
+ \exception
+ \test
+ \note
+ \attention
+ \sa
+ \remark 1.2011.01.08 21:14 created by Reyoung
+ \return QString
+*/
+        QString version()const;
+//! 返回Plugin的作者.
+/*!
+ \author Reyoung
+ \date 2011.01.08
+ \version ver 0.0.1
+  -
+ \exception
+ \test
+ \note
+ \attention
+ \sa
+ \remark 1.2011.01.08 21:15 created by Reyoung
+ \return QString
+*/
+        QString author()const;
+//! 返回Plugin的Bug Report E-mail.
+/*!
+ \author Reyoung
+ \date 2011.01.08
+ \version ver 0.0.1
+  -
+ \exception
+ \test
+ \note
+ \attention
+ \sa
+ \remark 1.2011.01.08 21:14 created by Reyoung
+ \return QString
+*/
+        QString bugReportEmail()const;
+
+//! 返回该Plugin依赖的Plugin.
+/*!
+ \author Reyoung
+ \date 2011.01.08
+ \version ver 0.0.1
+  -
+ \exception
+ \test
+ \note
+ \attention
+ \sa
+ \remark 1.2011.01.08 21:15 created by Reyoung
+ \return QList
+*/
+        QList<SpecDependencyData> dependencyList()const;
+
+        QString description()const;
+        QString category()const;
+        QString copyRight()const;
+        QString license()const;
     signals:
 
     private:
