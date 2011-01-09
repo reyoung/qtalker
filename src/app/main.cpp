c337d80f4fafe8567e201b04d3d6f566ad9f45a6
reyoung@126.com <reyoung@126.com@a30f7099-0f1c-977c-82b7-a2b34129996d>
2011-01-09 03:43:31 +0000

    Project File Maked. 使用根目录下的qtalker.pro 就是整体的Project file

---
+++ src/app/main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QtCore>
+#include <pluginmanager.h>
+int main(int argc,char** argv)
+{
+    QApplication app(argc,argv);
+
+    ExtensionSystem::PluginManager* pluginManager
+            =ExtensionSystem::PluginManager::instance();
+
+
+
+    return app.exec();
+}
