3301dd9ac2740559cf691271ecce0414c21de764
reyoung@126.com <reyoung@126.com@a30f7099-0f1c-977c-82b7-a2b34129996d>
2011-01-12 12:52:15 +0000

    Tcp Thread Complete. and well tested.

--- src/plugins/HelloWorldPlugin/helloworldplugin.cpp
+++ src/plugins/HelloWorldPlugin/helloworldplugin.cpp
@@ -8,6 +8,9 @@
 #include <sidewidget.h>
 #include "hellopage.h"
 #include "testhellomessage.h"
+#include <Network/tcpthread.h>
+
+const quint16 port[] = {5555,6666,7777,8888};
 
 HelloWorldPlugin::HelloWorldPlugin() :
         ExtensionSystem::IPlugin() {
@@ -22,11 +25,24 @@ bool HelloWorldPlugin::Initialize(const QStringList& cmdArgs) {
     WidgetFactory::instance()->getMainSideWidget()->addSidePage(&hp);
 
     this->m_udpthread = new Network::UdpThread(this);
+    this->m_tcpthread = new Network::TcpThread(this);
+//    this->connect(this->m_udpthread
+//                  ,SIGNAL(recieved(Network::RawData)),this,SLOT(RawRecv(Network::RawData)));
+    this->connect(this->m_tcpthread,SIGNAL(recieved(Network::RawData)),this,SLOT(RawRecv(Network::RawData)));
 
-    this->connect(this->m_udpthread
-                  ,SIGNAL(recieved(Network::RawData)),this,SLOT(RawRecv(Network::RawData)));
+    this->m_tcpthread->start();
     this->m_udpthread->start();
     this->m_udpthread->listen(4000);
+
+    for(int i=0;i<4;++i)
+    {
+        if(this->m_tcpthread->listen(port[i]))
+        {
+            qDebug()<<"Listen to "<<port[i];
+            break;
+        }
+    }
+
     QTimer* timer = new QTimer(this);
     this->connect(timer,SIGNAL(timeout()),this,SLOT(send()));
     timer->start(1000);
@@ -37,15 +53,21 @@ bool HelloWorldPlugin::Initialize(const QStringList& cmdArgs) {
 
 void HelloWorldPlugin::RawRecv(Network::RawData data)
 {
-    HelloPlugin::TestHelloMessage* ptr = Message::parse<HelloPlugin::TestHelloMessage>(data.Data);
-    qDebug()<<ptr->getRequestID();
-    delete ptr;
+    qDebug()<<"Tcp Recv";
+//    HelloPlugin::TestHelloMessage* ptr = Message::parse<HelloPlugin::TestHelloMessage>(data.Data);
+//    qDebug()<<ptr->getRequestID();
+//    delete ptr;
 }
 
 void HelloWorldPlugin::send()
 {
     HelloPlugin::TestHelloMessage msg;
     this->m_udpthread->broadcast(msg.toByteArray(),4000);
+    for(int i=0;i<4;++i)
+    {
+        Network::RawData d(QHostAddress::LocalHost,port[i],"Hello world");
+        this->m_tcpthread->send(d);
+    }
 }
 
 Q_EXPORT_PLUGIN2(HelloWorldPlugin, HelloWorldPlugin)
